88a794b3c0d3a5629300ad6e8e1310ef6f338299
Viktoria <zaharova181114@gmail.com>
2021-01-27 17:57:31 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,86 @@
+// tabs
+$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+    $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+});
+
+// slider cube
+var swiper = new Swiper('.home-slider', {
+    direction: 'vertical',
+    effect: 'cube',
+    grabCursor: true,
+    mousewheel: true,
+    speed: 750,
+    cubeEffect: {
+        shadow: false,
+        slideShadows: false,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    breakpoints: {
+        0: {
+            direction: 'horizontal',
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+        },
+        575: {
+            effect: 'cube',
+            direction: 'vertical',
+        }
+    }
+});
+
+var swiper = new Swiper('.design-slider', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    // slidesPerView: 3,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// mobile menu
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
+
+// animate scroll
+$('.go_to').on("click", function () {
+    let scroll_el = $(this).attr('href');
+    if ($(scroll_el).length !== 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    $('.mobile-menu').fadeOut();
+    return false;
+});
+
+// click tab = visible block
+$(".js-tab-trigger").on("click", function () {
+    let id = $(this).attr('data-tab'),
+        content = $('.js-tab-content[data-tab="'+ id +'"]');
+
+    $('.js-tab-trigger.active').removeClass('active'); // 1
+    $(this).addClass('active'); // 2
+
+    $('.js-tab-content.active').removeClass('active'); // 3
+    content.addClass('active'); // 4
+});
